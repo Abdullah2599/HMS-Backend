8a79c30066ef5d7f3d2588bc1d82e95bdee4479f
usman200440 <usmaniqra2004@gmail.com>
2025-01-13 22:04:33 -0800

    update

--- models/Room.js
+++ models/Room.js
@@ -3,6 +3,8 @@ const {Schema} = mongoose;
 
 const PermissionSchema= new Schema({
     room_type:({type:String,unique:true,required:true}),
+    avaibility:({type:String,enum:["available","disabled","in_work"]}),
+    size:({type:Number})
     
 })
 PermissionSchema.index({role:1,permission_list:1},{unique:true})
