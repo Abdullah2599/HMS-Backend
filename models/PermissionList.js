ec68b1660f47de77ac6fb620dce4046b74cb26f0
usman200440 <usmaniqra2004@gmail.com>
2025-01-28 07:02:28 -0800

    update

--- models/PermissionList.js
+++ models/PermissionList.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const {Schema} = mongoose;
 
 const PermissionListSchema= new Schema({
-    Permission_name:({type:String,required:true}),
+    Permission_name:({type:String,required:true,enum:[""]}),
 })
 const permission_list = mongoose.model("permission_list",PermissionListSchema);
 module.exports=permission_list;
